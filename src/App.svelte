914d3cad60e2f479e58cda1464b0e1d18a0ba2d8
BredanKM <bredankm@gmail.com>
2025-06-06 14:03:30 -0500

    style: add new note screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,22 @@
 <script>
 	import Content from "./lib/Components/Content.svelte";
+	import NewNoteScreen from "./lib/Components/NewNoteScreen.svelte";
 
 	let contentContainerRef;
 
 </script>
 
 <main class="relative flex h-full">
+	
+	
 	<div class="left-container min-w-1/6 bg-background-secondary">
     <div class="recent-folder-container">
     </div>
+	
+	 
   </div>
+  <NewNoteScreen />
+  <!-- 
 	<Content bind:this={contentContainerRef} />
+	-->
 </main>
---
+++ src/lib/Components/NewNoteScreen.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class="relative w-full h-screen flex flex-col justify-center items-center">
+    <div class="mb-6">
+        <div class="text-3xl">No file is open</div>
+    </div>
+    <div class="mb-4 text-md">
+        <button class="text-primary hover:cursor-pointer">
+            Create new note (Crtl + N)
+        </button>
+    </div>
+    <div class="mb-4">
+        <a href="/">See recent files (Crtl + O)</a>
+    </div>
+    <div class="close">
+        <a href="/">Close</a>
+    </div>
+</div>
