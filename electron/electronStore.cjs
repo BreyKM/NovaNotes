2cb0104b6180bde3620607dee4bdee2b89c4d91c
BredanKM <bredankm@gmail.com>
2026-09-12 22:11:32 -0500

    chore: set up electron typescript build pipeline via electronStore conversion

--- electron/electronStore.cjs
+++ electron/electronStore.cjs
@@ -1,40 +1,34 @@
+"use strict";
 const fs = require("fs");
 const path = require("path");
 const { app } = require("electron");
-
 class ElectronStore {
-  constructor(filename = "store.json") {
-    const userDataPath = app.getPath("userData");
-    this.filepath = path.join(userDataPath, filename);
-
-    try {
-      this.data = JSON.parse(fs.readFileSync(this.filepath, "utf-8"));
-      console.log(app.getPath("userData"));
-    } catch (error) {
-      // If file read or parse fails, start with an empty object
-      this.data = {};
+    filepath;
+    data;
+    constructor(filename = "store.json") {
+        const userDataPath = app.getPath("userData");
+        this.filepath = path.join(userDataPath, filename);
+        try {
+            this.data = JSON.parse(fs.readFileSync(this.filepath, "utf-8"));
+        }
+        catch (error) {
+            // If file read or parse fails, start with an empty object
+            this.data = {};
+        }
     }
-  }
-
-  get(key) {
-    return this.data[key];
-  }
-
-  set(key, value) {
-    this.data[key] = value;
-    console.log(app.getPath("userData"));
-    this.save();
-  }
-
-  delete(key) {
-    delete this.data[key];
-    this.save();
-  }
-
-  save() {
-    fs.writeFileSync(this.filepath, JSON.stringify(this.data));
-    console.log(app.getPath("userData"));
-  }
+    get(key) {
+        return this.data[key];
+    }
+    set(key, value) {
+        this.data[key] = value;
+        this.save();
+    }
+    delete(key) {
+        delete this.data[key];
+        this.save();
+    }
+    save() {
+        fs.writeFileSync(this.filepath, JSON.stringify(this.data));
+    }
 }
-
 module.exports = ElectronStore;
