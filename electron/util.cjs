1b58abea9ec1dd6b40621caefef5074a0be013db
BredanKM <84152232+BreyKM@users.noreply.github.com>
2026-09-12 21:18:49 -0500

    fix: use stable UUIDs for note identity instead of filesystem inode (#5)
    * fix: add missing file extensions to lint-staged glob
    
    * fix: use stable UUIDs for note identity instead of filesystem inode

--- electron/util.cjs
+++ electron/util.cjs
@@ -1,7 +1,15 @@
-const { ensureDir, writeFile, readdir, stat, readJSON } = require("fs-extra");
+const {
+  ensureDir,
+  writeFile,
+  readdir,
+  stat,
+  readJSON,
+  writeJSON,
+} = require("fs-extra");
 const { fileEncoding } = require("../shared/constants.cjs");
 const { dialog } = require("electron");
 const path = require("path");
+const { randomUUID } = require("crypto");
 
 const fse = require("fs-extra");
 
@@ -13,6 +21,22 @@ module.exports.updateNewNotebookDirPathMain = async (newPath) => {
 
 module.exports.updateActiveFolderPathInUtil = async (newPath) => {
   activeFolderPath = await Promise.resolve(newPath);
+};
+
+const INDEX_FILENAME = ".novanotes-index.json";
+
+const getIndexPath = (rootDir) => path.join(rootDir, INDEX_FILENAME);
+
+const loadIndex = async (rootDir) => {
+  try {
+    return await readJSON(getIndexPath(rootDir));
+  } catch (err) {
+    return {};
+  }
+};
+
+const saveIndex = async (rootDir, index) => {
+  await writeJSON(getIndexPath(rootDir), index, { spaces: 2 });
 };
 
 const getRootDir = () => {
@@ -79,56 +103,90 @@ module.exports.createWelcomeNote = async (welcomeNote, store) => {
 
 module.exports.getNotes = async (store) => {
   const rootDir = store.get("activeNotebookPath");
-  console.log("getNotes RootDir: ", rootDir);
 
   const notesFileNames = await readdir(rootDir, {
     encoding: fileEncoding,
     withFileTypes: false,
   });
 
-  console.log(
-    "MAIN-UTIL notesFileNameLength: ",
-    await Promise.resolve(notesFileNames),
+  const noteFiles = notesFileNames.filter((filename) =>
+    filename.endsWith(".md"),
   );
-  if (
-    (await Promise.resolve(notesFileNames).length) == 0 ||
-    !notesFileNames ||
-    notesFileNames == undefined ||
-    notesFileNames == null
-  ) {
-    return Promise.resolve([]);
-  } else {
-    const notes = await notesFileNames.filter((filename) =>
-      filename.endsWith(".md"),
-    );
-    console.log("notes: ", Promise.resolve(notes));
 
-    return Promise.all(notes.map(getNoteInfo(rootDir)));
+  const index = await loadIndex(rootDir);
+
+  const IndexSizeBefore = Object.keys(index).length;
+
+  const notes = await Promise.all(noteFiles.map(getNoteInfo(rootDir, index)));
+
+  if (Object.keys(index).length !== IndexSizeBefore) {
+    await saveIndex(rootDir, index);
   }
+
+  return notes;
 };
 
-const getNoteInfo = (rootDir) => async (filename) => {
+const getNoteInfo = (rootDir, index) => async (filename) => {
   const fileStats = await stat(`${rootDir}/${filename}`);
+  const title = filename.replace(/\.md$/, "");
+
+  if (!index[title]) {
+    index[title] = randomUUID();
+  }
 
   return {
-    title: filename.replace(/\.md$/, ""),
+    title,
     creationTime: fileStats.birthtimeMs,
     lastEditTime: fileStats.mtimeMs,
-    id: fileStats.ino,
+    id: index[title],
   };
 };
 
-module.exports.createNote = (file) => {
+module.exports.createNote = async (file) => {
   const rootDir = getRootDir();
-  console.log("createNote rootDir ", rootDir);
-  console.log("createNote filename ", file.title);
-  console.log("createNote content ", file.content);
 
-  console.log("createNote noteContent ", file.content);
+  const index = await loadIndex(rootDir);
+  index[file.title] = randomUUID();
+  await saveIndex(rootDir, index);
 
   return writeFile(`${rootDir}/${file.title}.md`, file.content, {
     encoding: fileEncoding,
   });
+};
+
+module.exports.renameNote = async (oldTitle, newTitle) => {
+  const rootDir = getRootDir();
+  if (!rootDir) {
+    console.error("renameNote called before rootDir is set.");
+    return false;
+  }
+
+  const oldPath = path.join(rootDir, `${oldTitle}.md`);
+  const newPath = path.join(rootDir, `${newTitle}.md`);
+
+  try {
+    await fse.access(newPath, fse.constants.F_OK);
+    console.error(
+      `Rename failed: A file named "${newTitle}.md" already exists.`,
+    );
+    return false;
+  } catch (error) {
+    try {
+      await fse.rename(oldPath, newPath);
+
+      const index = await loadIndex(rootDir);
+      if (index[oldTitle]) {
+        index[newTitle] = index[oldTitle];
+        delete index[oldTitle];
+        await saveIndex(rootDir, index);
+      }
+
+      return true;
+    } catch (renameError) {
+      console.error("Error during file rename operation: ", renameError);
+      return false;
+    }
+  }
 };
 
 module.exports.writeNote = (filename, content) => {
