de3ca6eba93416cc711ef1ab0a2c8c53518a03a7
BredanKM <bredankm@gmail.com>
2026-09-18 22:27:40 -0500

    fix(editor): give each loaded note a fresh editor state
    Replacing the document inside one state kept the previous note's undo history,
    so Ctrl+Z could write another note's text into the current file.

---
+++ src/lib/Components/Content/editor/noteEditor.ts
@@ -0,0 +1,45 @@
+import { EditorState, type Extension } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import {
+  defaultKeymap,
+  history,
+  historyKeymap,
+  indentWithTab,
+} from "@codemirror/commands";
+import { markdown } from "@codemirror/lang-markdown";
+import { indentUnit, syntaxHighlighting } from "@codemirror/language";
+import { livePreview } from "./livePreview";
+import { openLinkOnClick } from "./linkClick";
+import { autoPair } from "./autoPairs";
+import { indentKeymap } from "./indentKeymap";
+import { codeHighlight } from "./codeBlocks";
+import { resolveLanguage } from "./codeLanguages";
+import { update } from "lodash";
+
+export function noteExtensions(onEdit: (text: string) => void): Extension[] {
+  return [
+    history(),
+    keymap.of([...defaultKeymap, ...historyKeymap, indentWithTab]),
+    indentUnit.of("    "),
+    markdown({ codeLanguages: resolveLanguage }),
+    syntaxHighlighting(codeHighlight),
+    livePreview,
+    openLinkOnClick,
+    autoPair,
+    indentKeymap,
+    EditorView.lineWrapping,
+    EditorView.updateListener.of((update) => {
+      if (update.docChanged) {
+        onEdit(update.state.doc.toString());
+      }
+    }),
+  ];
+}
+
+export function loadNote(
+  view: EditorView,
+  text: string,
+  extensions: Extension[],
+): void {
+  view.setState(EditorState.create({ doc: text, extensions }));
+}
